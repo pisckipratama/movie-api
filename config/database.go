fb0c37d55b425b839ad9a47dd4023baad302852e
Asnur Ramdani <asnurramdhani12@gmail.com>
2023-05-20 15:01:14 +0700

    revisi get data from env

--- config/database.go
+++ config/database.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -10,11 +11,11 @@ import (
 func Connect() (*gorm.DB, error) {
 
 	//gorm connect Postgres
-	host := "localhost"
-	port := "5432"
-	user := "admin"
-	password := "123456"
-	dbname := "movie_db"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
 
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta", host, user, password, dbname, port)
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
